0d9fbc54a650e17bb1e6a74008dba90c85bce725
Felipe Freire <felipe.neves.freire@gmail.com>
2022-12-11 11:14:10 -0300

    implementacao entrada de dados

--- AlbumDeFigurinhas/src/modelo/Figurinha.java
+++ AlbumDeFigurinhas/src/modelo/Figurinha.java
@@ -2,4 +2,67 @@ package modelo;
 
 public class Figurinha {
 
+	private String nomeFigurinha, raridade, qualidadeFigurinha;
+	private int numeroFigurinha, qtdRepetidas, valorFigurinha;
+	
+	   public void apresentacao() {
+	        System.out.println("Nome das figurinha: " + nomeFigurinha);
+	        System.out.println("Raridade: " + raridade);
+	        System.out.println("Qualidade da figurinha: " + qualidadeFigurinha);
+	        System.out.println("Numero da figurinha: " + numeroFigurinha);
+	        System.out.println("Quantidade de repetidas: " + qtdRepetidas);
+	        System.out.println("Valor da figurinha: " + valorFigurinha);
+	        
+	    }
+
+	public String getNomeFigurinha() {
+		return nomeFigurinha;
+	}
+
+	public void setNomeFigurinha(String nomeFigurinha) {
+		this.nomeFigurinha = nomeFigurinha;
+	}
+
+	public String getRaridade() {
+		return raridade;
+	}
+
+	public void setRaridade(String raridade) {
+		this.raridade = raridade;
+	}
+
+	public String getQualidadeFigurinha() {
+		return qualidadeFigurinha;
+	}
+
+	public void setQualidadeFigurinha(String qualidadeFigurinha) {
+		this.qualidadeFigurinha = qualidadeFigurinha;
+	}
+
+	public int getNumeroFigurinha() {
+		return numeroFigurinha;
+	}
+
+	public void setNumeroFigurinha(int numeroFigurinha) {
+		this.numeroFigurinha = numeroFigurinha;
+	}
+
+	public int getQtdRepetidas() {
+		return qtdRepetidas;
+	}
+
+	public void setQtdRepetidas(int qtdRepetidas) {
+		this.qtdRepetidas = qtdRepetidas;
+	}
+
+	public int getValorFigurinha() {
+		return valorFigurinha;
+	}
+
+	public void setValorFigurinha(int valorFigurinha) {
+		this.valorFigurinha = valorFigurinha;
+	}
+	   
+	   
+	
 }
--- AlbumDeFigurinhas/src/view/TelaMenu.java
+++ AlbumDeFigurinhas/src/view/TelaMenu.java
@@ -1,5 +1,85 @@
 package view;
 
-public class TelaMenu {
+import java.util.Scanner;
+import modelo.Desenhos;
+
+public class TelaMenu extends Desenhos {
+
+	public static String opcaoMenu() {
+		// tentativa menu
+
+		Scanner scan = new Scanner(System.in);
+		String opcao;
+
+		System.out.println("------------------------");
+		System.out.println("          OPCAO         ");
+		System.out.println(" 1. Cadastrar Album     ");
+		System.out.println(" 2. Alterar Album       ");
+		System.out.println(" 3. Listar Album        ");
+		System.out.println(" 4. Deletar Album   " + "\n");
+
+		System.out.println(" 5. Cadastrar Figurinha ");
+		System.out.println(" 6. Alterar Figurinha   ");
+		System.out.println(" 7. Listar Figurinha    ");
+		System.out.println(" 8. Deletar Figurinha   ");
+		System.out.println(" 9.        SAIR         ");
+		opcao = scan.nextLine();
+		return opcao;
+	}
+
+	public static void codigo() {
+
+		Desenhos desenhos = new Desenhos();
+		int end = 0; 
+		Scanner scan = new Scanner(System.in);
+
+		do {
+			switch (opcaoMenu()) {
+			case "1":
+				// Cadastrar:
+				System.out.println("Cadastrar Album: ");
+				System.out.println("Nome Album: ");
+				desenhos.setNomeDoAlbum(scan.nextLine());
+				
+		        System.out.println(desenhos.getNomeDoAlbum());
+		        end = 1;
+				break;
+			case "2":
+
+				break;
+
+			case "3":
+
+				break;
+
+			case "4":
+
+				break;
+
+			case "5":
+
+				break;
+				
+			case "6":
+
+				break;
+
+			case "7":
+
+				break;
+
+			case "8":
+
+				break;	
+			}
+		} while (end < 9);
+
+	}
+
+	public static void main(String[] args) {
+
+		opcaoMenu();
+		codigo();
+	}
 
 }
