07da6d41d526f8c1a88fa2bc81bbea95ad72ff85
Felipe Freire <felipe.neves.freire@gmail.com>
2022-12-11 12:20:07 -0300

    implementado identificador para facilitar a alteracao

--- AlbumDeFigurinhas/src/view/TelaMenu.java
+++ AlbumDeFigurinhas/src/view/TelaMenu.java
@@ -79,6 +79,50 @@ public class TelaMenu extends Desenhos {
 				//System.out.println(desenhos.getNomeDoAlbum());
 				
 				id = i;
+				album[i][0] = "Identificador: " + Integer.toString(id);
+				album[i][1] = "Nome Album: " + desenhos.getNomeDoAlbum();
+				album[i][2] = "Editora do Album: " + desenhos.getEditora();
+				album[i][3] = "Pais da editora: " + desenhos.getPaisEditora();
+				album[i][4] = "Edicao: " + desenhos.getEdicao();
+				album[i][5] = "Pais origem: " + desenhos.getPaisOrigem();
+				album[i][6] = "Data de lancamento: " + desenhos.getDataLancamento();
+				album[i][7] = "Tipo da animacao: " + desenhos.getTipoAnimacao();
+				album[i][8] = "Franquia da animacao: " + desenhos.getFranquiaAnimacao()  + "\n";
+				i++;
+				end = 1;
+				System.out.println("\n Registrado com sucesso! \n");
+				break;
+			case "2":
+				// Alterar Album:
+				System.out.println("ALTERAR ALBUM: ");
+				System.out.println("INFORME O IDENTIFICADOR DO ALBUM: ");
+				
+				
+				
+				System.out.println("Editora do Album: ");
+				desenhos.setEditora(scan.nextLine());
+				
+				System.out.println("Pais da editora: ");
+				desenhos.setPaisEditora(scan.nextLine());
+				
+				System.out.println("Edicao: ");
+				desenhos.setEdicao(scan.nextLine());
+				
+				System.out.println("Pais origem: ");
+				desenhos.setPaisOrigem(scan.nextLine());
+				
+				System.out.println("Data de lancamento: ");
+				desenhos.setDataLancamento(scan.nextLine());
+				
+				System.out.println("Tipo da animacao: ");
+				desenhos.setTipoAnimacao(scan.nextLine());
+				
+				System.out.println("Franquia da animacao: ");
+				desenhos.setFranquiaAnimacao(scan.nextLine());
+
+				//System.out.println(desenhos.getNomeDoAlbum());
+				
+				id = i;
 				album[i][0] = "Nome Album: " + desenhos.getNomeDoAlbum();
 				album[i][1] = "Editora do Album: " + desenhos.getEditora();
 				album[i][2] = "Pais da editora: " + desenhos.getPaisEditora();
@@ -87,12 +131,8 @@ public class TelaMenu extends Desenhos {
 				album[i][5] = "Data de lancamento: " + desenhos.getDataLancamento();
 				album[i][6] = "Tipo da animacao: " + desenhos.getTipoAnimacao();
 				album[i][7] = "Franquia da animacao: " + desenhos.getFranquiaAnimacao()  + "\n";
-				i++;
-				end = 1;
-				System.out.println("\n Registrado com sucesso! \n");
-				break;
-			case "2":
-				// Alterar Album:
+				
+				
 				end = 2;
 				break;
 
@@ -102,7 +142,7 @@ public class TelaMenu extends Desenhos {
 				for(int lista = 0; lista < album.length; lista++) {
 					for(int p3 = 0; p3 < album[p3].length; p3++) {
 						if(album[lista][p3] != null) {
-							if(p3 < 8) {
+							if(p3 < 9) {
 							System.out.println("- " + album[lista][p3]);
 							}
 						}else {
